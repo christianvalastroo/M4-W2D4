aa0d3b0ebe3018689ebd5df1e9947288edc73f17
Christian Valastro <christianvalastro@icloud.com>
2026-03-27 15:43:53 +0100

    Corregge il selettore e avvia il rendering dei libri

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 const url = "https://striveschool-api.herokuapp.com/books"
 
-const booksContainer = document.querySelector("booksContainer")
+const booksContainer = document.querySelector("#booksContainer")
 
 const getBooks = async () => {
     try {
         const response = await fetch(url)
         const books = await response.json()
+        console.log(books)
 
-        console.log(response)
+        books.forEach(book => {
+            console.log(book.title)
+
+            const col = document.createElement("div")
+            col.setAttribute("class", "col-md-3")
+            booksContainer.append(col)
+        })
 
     } catch (error){
         console.error(error)
     }
 } 
 getBooks()
-
